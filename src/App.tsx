17ebce418d465844c4d5b126b13cb69018b129f7
DESKTOP-S17ROS1\Brandon <bchung@ucinnovation.com>
2024-09-11 14:42:46 -0700

    Background color picker added to settings button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Routes, Route, Navigate } from "react-router-dom"
-import { Container } from "react-bootstrap"
+import { Container, Form, Modal, Col, Row, Button, ButtonGroup } from "react-bootstrap"
 import { NewNote } from "./models/NewNote"
 import { useLocalStorage } from "./helper/useLocalStorage"
 import { useMemo } from "react"
@@ -9,6 +9,9 @@ import { NoteList } from "./models/NoteList"
 import { NoteLayout } from "./models/NoteLayout"
 import { Note } from "./models/Note"
 import { EditNote } from "./models/EditNote"
+import { useState, useEffect } from "react"
+import { PhotoshopPicker  } from "react-color"
+import styles from './App.module.css'
 
 export type Note = {
     id: string
@@ -36,9 +39,17 @@ export type Tag = {
     label: string
 }
 
+
+
 function App() {
     const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
     const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+    const [backgroundColor, setBackgroundColor] = useState('#fff');
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.style.backgroundColor = backgroundColor;
+    }, [backgroundColor]);
 
     const notesWithTags = useMemo(() => {
         return notes.map(note => {
@@ -46,13 +57,13 @@ function App() {
         })
     }, [notes, tags])
 
-    function onCreateNote({ tags, ...data}: NoteData) {
+    function onCreateNote( { tags, ...data }: NoteData ) {
         setNotes(prevNotes => {
             return [...prevNotes, {...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)}]
         })
     }
 
-    function onEditNote(id: string, { tags, ...data}: NoteData) {
+    function onEditNote( id: string, { tags, ...data }: NoteData ) {
         setNotes(prevNotes => {
             return prevNotes.map(note => {
                 if(note.id === id){
@@ -65,19 +76,19 @@ function App() {
         })
     }
     
-    function onDeleteNote(id: string) {
+    function onDeleteNote( id: string ) {
         setNotes(prevNotes => {
             return prevNotes.filter(note => note.id != id)
         })
     }
 
-    function onAddTag( data : Tag){
+    function onAddTag( data : Tag ){
         setTags(prevTags => {
             return [...prevTags, data]
         })
     }
 
-    function onUpdateTag( data : Tag){
+    function onUpdateTag( data : Tag ){
         setTags(prevTags => {
             return prevTags.map(tag => {
                 if(tag.id == data.id){
@@ -88,14 +99,86 @@ function App() {
         })
     }
 
-    function onDeleteTag( id : string){
+    function onDeleteTag( id : string ){
         setTags(prevTags => {
             return prevTags.filter(tag => tag.id != id)
         })
     }
+        
+    type SettingsModalProps = {
+        show: boolean,
+        backgroundColor: string,
+        setBackgroundColor: (id: string) => void,
+        setModalIsOpen: (state: boolean) => void
+    }
+
+    function SettingsModal( props : SettingsModalProps ) {
+        interface colorObj {
+            hex: string
+        }
+
+        const [currColor, setCurrColor] = useState(props.backgroundColor);
+        const [showBackgroundColorPicker, setShowBackgroundColorPicker] = useState(true);
+
+        const handleAccept = () => {
+            props.setBackgroundColor(currColor);
+        };
+        const handleChange = (color: colorObj) => {
+            setCurrColor(color.hex);
+        };
+        const handleCancel = () => {
+            setShowBackgroundColorPicker(false);
+        };
+
+        return <Modal size="lg" show={props.show} onHide={() => {props.setModalIsOpen(false)}} centered>
+            <Modal.Header style={{ background: currColor }} closeButton>
+                {/* <Modal.Title>Edit Tags</Modal.Title> */}
+            </Modal.Header>
+            <Modal.Body style={{ background: currColor }} >
+                <Form>
+                    {/* i want column of buttons on the left and a panel on the right for info */}
+                    <Row>
+                        <Col xs={3}>
+                            <Row className="justify-content-center">
+                                <ButtonGroup vertical>
+                                    <div className={styles.button} style={{ background: 'grey' }}>Profile</div>
+                                    <div style={{ background: 'grey' }}>Themes</div>
+                                    <div style={{ background: 'grey' }}>Layout</div>
+                                    <Button>Profile</Button>
+                                    <Button>Themes</Button>
+                                    <Button>Layout</Button>
+                                </ButtonGroup>
+                            </Row>
+                        </Col>
+                        <Col xs={9}>
+                            <Row className="justify-content-center">
+                                 {showBackgroundColorPicker && (
+                                <PhotoshopPicker 
+                                    className={styles.photoshopPicker}
+                                    color={currColor}
+                                    onAccept={handleAccept}
+                                    onChange={handleChange}
+                                    onCancel={handleCancel}
+                                />
+                            )}
+                            </Row>
+                        </Col>
+                    </Row>
+                </Form>
+            </Modal.Body>
+        </Modal>
+    }
 
     return (
-        <Container style={{paddingTop:16}}>
+        <Container style={{paddingTop:32}}>
+            <Container>
+                <Row >
+                    <Col className="p-0">
+                        <Button variant="outline-secondary" className="float-end" onClick={() => setModalIsOpen(true)}>Settings</Button>
+                    </Col>
+                </Row>
+            </Container>
+            <SettingsModal show={modalIsOpen} backgroundColor={backgroundColor} setBackgroundColor={setBackgroundColor} setModalIsOpen={() => setModalIsOpen(false)}/>
             <Routes>
                 <Route path="/" element={<NoteList onUpdateTag={onUpdateTag} onDeleteTag={onDeleteTag} availableTags={tags} notes={notesWithTags}/>}/>
                 <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={onAddTag} availableTags={tags}/>}/>
--- src/models/NoteCard.tsx
+++ src/models/NoteCard.tsx
@@ -12,7 +12,7 @@ type NoteCardProps = {
 export function NoteCard({ id, title, tags }: NoteCardProps){
 
     return <>
-        <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+        <Card as={Link} to={`/${id}/edit`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
             <Card.Body>
                 <Stack gap={2} className="align-items-center justify-content-center h-100">
                     <span className="fs-5">{title}</span>
